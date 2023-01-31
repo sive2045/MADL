2c211a3aedfabd470992d5d6d5f8725ff17f49c0
sive2045 <lc9902130509@gmail.com>
2023-02-01 03:56:07 +0900

    feat: zoo

--- rllib/Custom-Enviroment/custom-environment/envs/LEO_SAT_envronment.py
+++ rllib/Custom-Enviroment/custom-environment/envs/LEO_SAT_envronment.py
@@ -9,4 +9,18 @@ from pettingzoo.utils.env import ParallelEnv
 
 class LEOSATEnv(ParallelEnv):
     def __init__(self) -> None:
-        super().__init__()
+        self.area_max_x = 100    # km
+        self.area_max_y = 100    # km
+        self.area_max_z = 1_000  # km
+
+        self.GS_x = None # km
+        self.GS_y = None # km
+        self.GS_z = 0    # km
+
+        self.SBS_x = None # km
+        self.SBS_y = None # km
+        self.SBS_z = 540  # km
+
+        self.timestep = None
+
+        self.possible_agents = ["groud_station", "satellite_basesation"]
