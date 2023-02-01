1cb9af825f849d744c96cd0e99d298fc823528d4
sive2045 <lc9902130509@gmail.com>
2023-02-01 18:39:16 +0900

    feat: ZOO

--- rllib/Custom-Enviroment/custom-environment/envs/LEO_SAT_envronment.py
+++ rllib/Custom-Enviroment/custom-environment/envs/LEO_SAT_envronment.py
@@ -24,3 +24,28 @@ class LEOSATEnv(ParallelEnv):
         self.timestep = None
 
         self.possible_agents = ["groud_station", "satellite_basesation"]
+    
+    def reset(self, seed=None, return_info=False, options=None):
+        self.agents = copy(self.possible_agents)
+
+        self.timestep = 0
+
+        self.GS_x = np.random.randint(0,100 + 1) # (0~101]
+        self.GS_y = np.random.randint(0,100 + 1)
+        self.GS_z = 0
+
+        self.SBS_x = 0 + np.random.normal(0,1)
+        self.SBS_y = 0 + np.random.normal(0,1)
+        self.SBS_z = 540 + np.random.normal(0,1)
+
+        observation = (
+            [self.GS_x, self.GS_y, self.GS_z],
+            [self.SBS_x, self.SBS_y, self.SBS_z]
+        )
+        observations = {
+            "ground_stations": observation,
+            "satellite_basestation": observation
+        }
+
+        return observations
+        
