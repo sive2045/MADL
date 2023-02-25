a87463ae61022df676b244fb870fa58e7a9f0335
sive2045 <lc9902130509@gmail.com>
2023-02-25 17:05:07 +0900

    feat: SAT

--- rllib/Custom-Enviroment/custom-environment/envs/LEO_SAT_envronment.py
+++ rllib/Custom-Enviroment/custom-environment/envs/LEO_SAT_envronment.py
@@ -16,16 +16,18 @@ class LEOSATEnv(ParallelEnv):
         self.GS_size = 10
         self.GS = np.zeros((self.GS_size, 3)) # coordinate (x, y, z) of GS
 
-        self.SAT_len = 130
+        self.SAT_len = 22
+        self.SAT_plane = 2 # of plane
         self.SAT_coverage_radius = 55 # km
         self.SAT_speed = 7.9 # km/s
         self.theta = np.linspace(0, 2 * np.pi, 150)
-        self.SAT_0 = np.zeros((self.SAT_len, 3, 150)) # coordinate (x, y, z) of SAT (plane 1)
-        self.SAT_1 = np.zeros((self.SAT_len, 3, 150)) # coordinate (x, y, z) of SAT (plane 2)
-        self.SAT_0[:,2,:] = 500 # km, SAT height 
-        self.SAT_1[:,2,:] = 500 # km, SAT height 
+        self.SAT_point = np.zeros((self.SAT_len * self.SAT_plane, 3)) # coordinate (x, y, z) of SAT center point
+        self.SAT_coverage = np.zeros((self.SAT_len * self.SAT_plane, 3, 150)) # coordinate (x, y, z) of SAT coverage
+        self.SAT_point[:,2,:] = 500 # km, SAT height 
+        self.SAT_coverage[:,2,:] = 500 # km, SAT height
 
         self.timestep = None
+        self.terminal_time = 155 # s
 
         self.possible_agents = [
             "groud_station_00", "groud_station_01", "groud_station_02",
@@ -34,15 +36,35 @@ class LEOSATEnv(ParallelEnv):
             "groud_station_09"
             ]
     
-    def _SAT_location(self, SAT_0, SAT_1, SAT_len, time, speed, radius, theta):
+    def _SAT_coordinate(self, SAT, SAT_len, time, speed):
+        """
+        return real-time SAT center point
+        """
         for i in range(SAT_len):
-            SAT_0[i][0][:] = 65*i -speed * time + radius * np.cos(theta)
-            SAT_0[i][1][:] =  10                + radius * np.sin(theta)
+            SAT[i,0] = 65*i -speed * time
+            SAT[i,1] = 10
 
-            SAT_1[i][0][:] = -25 + 65*i -speed * time + radius * np.cos(theta)
-            SAT_1[i][1][:] =  10 +   65               + radius * np.sin(theta)
+            SAT[i + SAT_len,0] = -25 + 65*i -speed * time
+            SAT[i+ SAT_len,1] =  10 + 65
+        
+        return SAT
 
-        return SAT_0, SAT_1
+    def _SAT_coverage_position(self, SAT_coverage, SAT_len, time, speed, radius, theta):
+        """
+        return real-time SAT coverage position
+        for render
+        """
+        for i in range(SAT_len):
+            SAT_coverage[i,0,:] = 65*i -speed * time + radius * np.cos(theta)
+            SAT_coverage[i,1,:] =  10                + radius * np.sin(theta)
+
+            SAT_coverage[i + SAT_len,0,:] = -25 + 65*i -speed * time + radius * np.cos(theta)
+            SAT_coverage[i + SAT_len,1,:] =  10 +   65               + radius * np.sin(theta)
+
+        return SAT_coverage
+
+    def _is_in_coverage(self, SAT, GS):
+        pass
 
     def reset(self, seed=None, return_info=False, options=None):
         self.agents = copy(self.possible_agents)
@@ -55,8 +77,7 @@ class LEOSATEnv(ParallelEnv):
             self.GS[i][1] = np.random.randint(0,100 + 1)
 
         # set SAT position
-        self.SAT_0, self.SAT_1 = self._SAT_location(self.SAT_0, self.SAT_1, self.SAT_len, self.timestep, 
-                                                    self.SAT_speed, self.SAT_coverage_radius, self.theta)
+        self.SAT_point = self._SAT_coordinate(self.SAT_point, self.SAT_len, self.timestep, self.SAT_speed)
 
         GS_observation_info = {
             "coordinate": [self.GSs_x, self.GSs_y, self.GSs_z],
@@ -78,20 +99,10 @@ class LEOSATEnv(ParallelEnv):
 
     def step(self, actions):
         # Execute actions
-        GS_actions = actions["ground_stations"]          # 1: connect rq 0: backoff
-        SBS_actions = actions["satellite_basesations"]   # decision power
+        GS_actions = actions["ground_stations"]          # one-hot encoding
+        
 
-        for i in range(len(GS_actions)):
-            self.connected_GSs = GS_actions[i]
-            self.allocated_power = SBS_actions[i]
-
-            if self.connected_GSs:
-                pass
-            else:
-                if self.allocated_power > 0:
-                    SBS_reward = -1
-                else:
-                    SBS_reward = 0
+        
         
 
 
